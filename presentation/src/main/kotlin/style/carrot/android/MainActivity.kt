4cf19716c7c08b683471d9a0056d06ccb13d1f12
jisungbin <ji@sungb.in>
2022-01-23 00:08:49 +0900

    Update main activity

--- presentation/src/main/kotlin/style/carrot/android/MainActivity.kt
+++ presentation/src/main/kotlin/style/carrot/android/MainActivity.kt
@@ -12,21 +12,41 @@ package style.carrot.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import style.carrot.android.theme.CarrotStyleTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             CarrotStyleTheme {
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text(text = "Bye world.")
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 30.dp),
+                    floatingActionButton = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_round_add_24),
+                            contentDescription = null
+                        )
+                    }
+                ) {
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        style = MaterialTheme.typography.titleMedium
+                    )
                 }
             }
         }
